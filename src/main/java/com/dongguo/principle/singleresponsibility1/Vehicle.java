fc3a6d7f4f83b916cfdf857a57ee262bb5a9b209
dongguo4812 <291320608@qq.com>
2021-08-21 19:07:20 +0800

    设计模式初始化

---
+++ src/main/java/com/dongguo/principle/singleresponsibility1/Vehicle.java
@@ -0,0 +1,16 @@
+package com.dongguo.principle.singleresponsibility1;
+
+/**
+ * @author Dongguo
+ * @date 2021/8/21 0021-18:17
+ * @description:交通工具类
+ */
+class Vehicle {
+    /**
+     * @author Dongguo 
+     * @description: 方式1
+     */
+    public void run(String vehicle) {
+        System.out.println(vehicle + " 在公路上运行....");
+    }
+}
